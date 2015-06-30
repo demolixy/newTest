f065e1e4f90265211f84aab5e03ad5214b13fd6a
demolixy <demo_lxy@163.com>
2015-06-30 18:20:15 +0800

    first commit

---
+++ src/com/demo/test/Test.java
@@ -0,0 +1,15 @@
+package com.demo.test;
+
+/** 
+ * @author  Lixy 
+ * @date    2015-6-30 下午6:16:34 
+ * @version 1.0 
+ * @since  
+ */
+public class Test {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
