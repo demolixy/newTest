794233463fc76bbdc24c7326a7ba48ffb7b883b2
demolixy <demo_lxy@163.com>
2015-06-30 19:11:25 +0800

    gfgf

--- src/com/demo/test/Test.java
+++ src/com/demo/test/Test.java
@@ -9,6 +9,7 @@ package com.demo.test;
 public class Test {
     
     public static void main(String[] args) {
+        System.out.println("test");
         System.out.println("Hello World");
     }
 
