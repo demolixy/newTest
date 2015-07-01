df04a54cca90f7989ed4ab548c0c0a25b08cde8f
demolixy <demo_lxy@163.com>
2015-07-01 13:46:26 +0800

    第一次提交

--- src/com/demo/test/TestNIODemo.java
+++ src/com/demo/test/TestNIODemo.java
@@ -13,9 +13,13 @@ public class TestNIODemo {
     public static void main(String[] args) {
         //申请一个默认的的缓存区的大小
         IntBuffer buffer = IntBuffer.allocate(10);
+        //写入数据之前的limit  capacity position
+        System.out.println(buffer.limit() + "---" + buffer.capacity() + "---" + buffer.position());
         buffer.put(7);
         int[] temp = {5,6,6};
-        buffer.put(temp);
+        buffer.put(temp);//现在我们的缓冲区中存放的数据是四个记录
+        //写入数据之后的几种常量的变化
+        System.out.println(buffer.limit() + "---" + buffer.capacity() + "---" + buffer.position());
         
     }
 
